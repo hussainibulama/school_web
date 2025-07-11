6b7800bf24e60d707486e2ad155f12dc623e7706
Hussaini Bulama <hussainibulama@gmial.com>
2025-07-11 22:36:24 +0100

    z index

--- src/components/loader/index.tsx
+++ src/components/loader/index.tsx
@@ -19,7 +19,7 @@ export default function Loader() {
       left={0}
       right={0}
       bottom={0}
-      zIndex={10}
+      zIndex={20}
       display='flex'
       alignItems='center'
       justifyContent='center'
